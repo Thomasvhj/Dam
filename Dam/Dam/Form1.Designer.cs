27d58e601c566ac90a1de97c4ba69a24a94f5378
MassiveTonger <chri2b90@euclive.dk>
2024-10-01 13:58:37 +0200

    Yearh

--- Dam/Dam/Form1.Designer.cs
+++ Dam/Dam/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.G = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // G
+            // 
+            this.G.BackColor = System.Drawing.Color.Gray;
+            this.G.Location = new System.Drawing.Point(12, 27);
+            this.G.Name = "G";
+            this.G.Size = new System.Drawing.Size(1216, 854);
+            this.G.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Dam.Properties.Resources.W;
+            this.pictureBox1.Location = new System.Drawing.Point(1298, 252);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1630, 911);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.G);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel G;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Dam/Dam/Form1.cs
+++ Dam/Dam/Form1.cs
@@ -16,5 +16,19 @@ namespace Dam
         {
             InitializeComponent();
         }
+        int n;
+        PictureBox = new PictureBox[,] P;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
     }
 }
