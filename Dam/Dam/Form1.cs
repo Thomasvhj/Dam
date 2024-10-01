d29b08683ad2229bd2633f3b8038143a041712b6
MassiveTonger <chri2b90@euclive.dk>
2024-10-01 14:05:40 +0200

    hh

--- Dam/Dam/Form1.cs
+++ Dam/Dam/Form1.cs
@@ -16,19 +16,17 @@ namespace Dam
         {
             InitializeComponent();
         }
+
         int n;
         
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+        private void test()
+        {
 
-
-
-
-
-
-
+        }
 
     }
 }
